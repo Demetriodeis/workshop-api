3877f8812761fbe6c479a97244405ad1f45bab9b
Demetrio Pacheco <61946635+Demetriodeis@users.noreply.github.com>
2022-10-24 11:26:28 -0300

    first project

---
+++ Idez.Api.Workshop/Startup.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Idez.Api.Workshop
+{
+    public class Startup
+    {
+        private const string SWAGGERFILE_PATH = "./swagger/v1/swagger.json";
+        private const string API_VERSION = "v1";
+        private const string TITLE_PROJECT = "Workshop API";
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+                
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            AddSwagger(services);
+        }
+                
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger()
+                .UseSwaggerUI(c => c.SwaggerEndpoint(SWAGGERFILE_PATH, TITLE_PROJECT + API_VERSION));
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        public void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = TITLE_PROJECT, Version = API_VERSION });
+            });
+        }
+    }
+}
